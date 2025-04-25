a79ac3395c16d29d1ce86ef534aad859e5949abd
chedli01 <chedli.masmoudi01@gmail.com>
2025-04-25 21:00:16 +0100

    task 5

--- index.py
+++ index.py
@@ -2,22 +2,10 @@ import pandas as pd
 
 # Load the Excel file
 df = pd.read_excel("Exemple2Reference_SReg_PW2.xlsx")
+import statsmodels.api as sm
 
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+X_with_const = sm.add_constant(df['x'])
+regression = sm.OLS(df['y'], X_with_const).fit()
+anova_table = sm.stats.anova_lm(regression, typ=2)
 
-# Assuming df is already loaded with data
-X = df['x'].values.reshape(-1, 1)
-y = df['y'].values
-
-model = LinearRegression()
-model.fit(X, y)
-
-# Plot the data and regression line
-plt.scatter(df['x'], df['y'], color='blue', label='Data')
-plt.plot(df['x'], model.predict(X), color='red', label='Regression line')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.title('Regression Line')
-plt.legend()
-plt.show()
+print(anova_table)
